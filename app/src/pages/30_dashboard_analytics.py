1460a770fabda239ada076b98218a5d21337fae1
DunnyBunny1 <murray.don@northeastern.edu>
2025-04-15 13:42:58 -0400

    Created sidebar and added sidebar links to each streamlit page

--- app/src/pages/30_dashboard_analytics.py
+++ app/src/pages/30_dashboard_analytics.py
@@ -1,3 +1,6 @@
 import streamlit as st 
+from modules.nav import SideBarLinks
 
+# Display the appropriate sidebar links for the role of the logged in user
+SideBarLinks()
 st.write("Welcome to the dashboard feeds page")
